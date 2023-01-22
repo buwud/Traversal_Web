8abcf1cc8fc3ab417b3ff8885a58397c51bc291b
[buse duran] <buseduran500@gmail.com>
2023-01-22 12:11:08 +0300

    entity wip

---
+++ EntityLayer/Concrete/Destination.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+	internal class Destination
+	{
+		public int DestinationID { get; set; }
+		public string? City { get; set; }
+		public string? StayTime { get; set; }
+		public double? Price { get; set; }
+		public string? Image { get; set; }
+		public string? Description { get; set; }
+		public string? Capacity { get; set; }
+		public bool Status { get; set; }
+
+
+	}
+}
---
+++ EntityLayer/Concrete/Feature.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+	public class Feature
+	{
+		public int FeatureID { get; set; }
+		public string? post1Name  { get; set; }
+		public string? post1Description  { get; set; }
+		public string? post1Image  { get; set; }
+		public bool? post1Status  { get; set; }
+		//durumu true oılan 4 satır olcak
+
+
+		public int Feature2ID { get; set; }
+		public string? post2Name { get; set; }
+		public string? post2Description { get; set; }
+		public string? post2Image { get; set; }
+		public bool? post2Status { get; set; }
+
+
+
+
+	}
+}
