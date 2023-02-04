64ee6f01aa47d476c37beba26cf11566e3297eb1
[buse duran] <buseduran500@gmail.com>
2023-02-04 18:28:16 +0300

    services done

---
+++ Traversal/Controllers/DestinationController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager _destinationManager = new DestinationManager(new EfDestinationDal()); 
+        public IActionResult Index()
+        {
+            var values = _destinationManager.GetList();
+            return View(values);
+        }
+    }
+}
