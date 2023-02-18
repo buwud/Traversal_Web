da3643126845aea152d074c8a11acec3c68cd220
[buse duran] <buseduran500@gmail.com>
2023-02-18 15:37:59 +0300

    some bugs fixed

--- Traversal/Controllers/LoginController.cs
+++ Traversal/Controllers/LoginController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Traversal.Models;
 
 namespace Traversal.Controllers
@@ -19,41 +21,63 @@ namespace Traversal.Controllers
         [HttpGet]
         public IActionResult SignUp()
         {
-            return View();
+            var model = new UserRegisterViewModel();
+            model.GenderOptions = new List<SelectListItem>()
+            { 
+             new SelectListItem{Value=" ",Text="Select Gender"},
+             new SelectListItem{Value="Female",Text="Female"},
+             new SelectListItem{Value="Male",Text="Male"},
+             new SelectListItem{Value="Others",Text="Others"}
+            };
+            return View(model);
         }
+
         [HttpPost]
         public async Task<IActionResult> SignUp(UserRegisterViewModel m)
         {
-            AppUser user = new AppUser()
+            if (ModelState.IsValid)
             {
-                 Name= m.Name,
-                 SurName=m.Surname,
-                 Email=m.Email,
-                 PhoneNumber=m.Phone,
-                 Gender=m.Gender,
-                 Age=m.Age,
-                 UserName=m.Username
-            };
-
-            if (m.Password == m.ConfirmPassword)
-            {
-                var result = await _userManager.CreateAsync(user, m.Password);
-
-                if(result.Succeeded)
+                AppUser user = new AppUser()
                 {
-                    return RedirectToAction("SignIn");
-                }
-                else
+                    Name = m.Name,
+                    SurName = m.Surname,
+                    Email = m.Email,
+                    PhoneNumber = m.Phone,
+                    Gender = m.Gender,
+                    Age = m.Age,
+                    UserName = m.Username
+                };
+
+                if (m.Password == m.ConfirmPassword)
                 {
-                    foreach(var item in result.Errors)
+                    var result = await _userManager.CreateAsync(user, m.Password);
+
+                    if (result.Succeeded)
                     {
-                        ModelState.AddModelError("", item.Description);
+                        return RedirectToAction("SignIn");
+                    }
+                    else
+                    {
+                        foreach (var item in result.Errors)
+                        {
+                            ModelState.AddModelError("", item.Description);
+                        }
                     }
                 }
             }
+
+            m.GenderOptions = new List<SelectListItem>()
+            {
+            new SelectListItem{Value="",Text="Select Gender"},
+            new SelectListItem{Value="Female",Text="Female"},
+            new SelectListItem{Value="Male",Text="Male"},
+            new SelectListItem{Value="Others",Text="Others"}
+            };
+
             return View(m);
         }
-        
+
+
         public IActionResult SignIn()
         {
             return View();
--- Traversal/Models/UserRegisterViewModel.cs
+++ Traversal/Models/UserRegisterViewModel.cs
@@ -1,26 +1,42 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace Traversal.Models
 {
+    public class TurkishPhoneNumberAttribute : RegularExpressionAttribute
+    {
+        public TurkishPhoneNumberAttribute() : base(@"^(0|\+90)(\d{3})\)?[\s.-]?(\d{3})[\s.-]?(\d{2})[\s.-]?(\d{2})$")
+        {
+            ErrorMessage = "The phone number is not in a valid format!";
+        }
+    }
     public class UserRegisterViewModel
     {
         [Required(ErrorMessage ="Enter your name")]
+        [StringLength(42,MinimumLength =3,ErrorMessage = "Minimum 3 and maximum 42 characters allowed!")]
         public string? Name { get; set; }
 
         [Required(ErrorMessage = "Enter your surname")]
+        [StringLength(42, MinimumLength = 3, ErrorMessage = "Minimum 3 and maximum 42 characters allowed!")]
         public string? Surname { get; set; }
 
-        [Required(ErrorMessage = "Enter your email")]
+        [Required(ErrorMessage = "Enter your email")]//a@gmail.com
+        [StringLength(42, MinimumLength = 3, ErrorMessage = "Minimum 11 and maximum 42 characters allowed!")]
         public string? Email { get; set; }
 
         [Required(ErrorMessage = "Enter your username")]
+        [StringLength(42, MinimumLength = 3, ErrorMessage = "Minimum 3 and maximum 42 characters allowed!")]
         public string? Username { get; set; }
 
         [Required(ErrorMessage = "Enter your phone number")]
+        [StringLength(42, MinimumLength = 3, ErrorMessage = "Minimum 3 and maximum 42 characters allowed!")]
+        [TurkishPhoneNumber]
         public string? Phone { get; set; }
 
         [Required(ErrorMessage = "Select your gender")]
         public string? Gender { get; set; }
+
+        public List<SelectListItem>? GenderOptions { get; set; }
         
         [Required(ErrorMessage = "Enter your age")]
         [Range(18,150,ErrorMessage ="Enter your age correctly")]
