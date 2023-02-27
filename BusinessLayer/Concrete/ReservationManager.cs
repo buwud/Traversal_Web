af2fddc52b034eadaa93dc0ad9055ecc4155f855
[buse duran] <buseduran500@gmail.com>
2023-02-27 21:46:56 +0300

    reservation1

---
+++ BusinessLayer/Concrete/ReservationManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ReservationManager : IReservationService
+    {
+        IReservationDal _reservationDal;
+
+        public ReservationManager(IReservationDal reservationDal)
+        {
+            _reservationDal = reservationDal;
+        }
+
+        public List<Reservation> GetList()
+        {
+            return _reservationDal.GetList();
+        }
+
+        public void TDelete(Reservation t)
+        {
+            _reservationDal.Delete(t);
+        }
+
+        public Reservation TGetByID(int id)
+        {
+            return _reservationDal.GetValue(id);
+        }
+
+        public void TInsert(Reservation t)
+        {
+            _reservationDal.Insert(t);
+        }
+
+        public void TUpdate(Reservation t)
+        {
+            _reservationDal.Update(t);
+        }
+    }
+}
---
+++ Traversal/Areas/Member/Controllers/ReservationController.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Traversal.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class ReservationController : Controller
+    {
+        DestinationManager _destinationManager = new DestinationManager(new EfDestinationDal());
+        ReservationManager _reservationManager = new ReservationManager(new EfReservationDal());
+        //REFACTORİNG EYLENECEK
+        public IActionResult CurrReservation()
+        {
+
+            return View();
+        }
+
+        public IActionResult OldReservation()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult NewReservation()
+        {
+            List<SelectListItem> values = (from x in _destinationManager.GetList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.City,//destination tarafında city ismi kullanılacak
+                                               Value = x.DestinationID.ToString()
+                                           }).ToList();
+            ViewBag.values = values;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewReservation(Reservation r)
+        {
+            r.AppUserId = 3;
+            _reservationManager.TInsert(r);
+            return RedirectToAction("CurrReservation");
+        }
+    }
+}
