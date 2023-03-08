9b9a83845874bf41cda50a1c98216ae7fa99396d
[buse duran] <buseduran500@gmail.com>
2023-03-08 15:02:11 +0300

    member dashboard to partials

---
+++ Traversal/Areas/Member/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.Areas.Member.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [Area("Member")]
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
