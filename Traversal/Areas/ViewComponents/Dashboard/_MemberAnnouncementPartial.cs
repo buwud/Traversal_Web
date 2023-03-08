a63e71fa9247f7435e986bff03ff5bb77bd43847
[buse duran] <buseduran500@gmail.com>
2023-03-08 17:19:40 +0300

    member dashboard into sections done

---
+++ Traversal/Areas/ViewComponents/Dashboard/_MemberAnnouncementPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.Areas.ViewComponents.Dashboard
+{
+    public class _MemberAnnouncementPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Traversal/Areas/ViewComponents/Dashboard/_MemberHeadingPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.Areas.ViewComponents.Dashboard
+{
+    public class _MemberHeadingPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
