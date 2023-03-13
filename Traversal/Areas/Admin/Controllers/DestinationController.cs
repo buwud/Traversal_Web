629d32ebed8e1dfdd8c84a98915222a08c57ca94
[buse duran] <buseduran500@gmail.com>
2023-03-14 00:28:51 +0300

    admin add destination design done

--- Traversal/Areas/Admin/Controllers/DestinationController.cs
+++ Traversal/Areas/Admin/Controllers/DestinationController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Traversal.Areas.Admin.Controllers
@@ -12,6 +13,44 @@ namespace Traversal.Areas.Admin.Controllers
         {
             var values = _destinationManager.GetList();
             return View(values);
+        }
+        //Addition
+        [HttpGet]
+        public IActionResult AddDestination() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddDestination(Destination d)
+        {
+            _destinationManager.TInsert(d);
+            return RedirectToAction("Index");
+        }
+        //Deletion
+        [HttpGet]
+        public IActionResult DeleteDestination(int id)
+        {
+            var value = _destinationManager.TGetByID(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult DeleteDestination(Destination d)
+        {
+            _destinationManager.TDelete(d);
+            return RedirectToAction("Index");
+        }
+        //Edition
+        [HttpGet]
+        public IActionResult EditDestination(int id)
+        {
+            var value = _destinationManager.TGetByID(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult EditDestination(Destination d)
+        {
+            _destinationManager.TUpdate(d);
+            return RedirectToAction("Index");
         }
     }
 }
