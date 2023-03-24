d449ad6c94b6e46c7b37c248a5acc5853ac9241a
[buse duran] <buseduran500@gmail.com>
2023-03-24 14:51:42 +0300

    admin guest index but 5 buttons

--- Traversal/Areas/Admin/Controllers/GuestController.cs
+++ Traversal/Areas/Admin/Controllers/GuestController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Traversal.Areas.Admin.Controllers
@@ -18,5 +19,35 @@ namespace Traversal.Areas.Admin.Controllers
             var values = _appUserService.GetList();
             return View(values);
         }
+        public IActionResult DeleteGuest(int id)
+        {
+            var value = _appUserService.TGetByID(id);
+            _appUserService.TDelete(value);
+            string returnUrl = Request.Headers["Referer"].ToString();
+            return Redirect(returnUrl);
+        }
+        [HttpGet]
+        public IActionResult EditGuest(int id)
+        {
+            var values = _appUserService.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditGuest(AppUser a)
+        {
+            _appUserService.TUpdate(a);
+            return RedirectToAction("Index");
+        }
+        public IActionResult GuestComments()
+        {
+            _appUserService.GetList();
+            return View();
+        }
+        public IActionResult GuestHistory()
+        {
+            _appUserService.GetList();
+            return View();
+        }
+
     }
 }
