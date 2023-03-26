809f76274fa95051a0b2b61566b026367668b541
[buse duran] <buseduran500@gmail.com>
2023-03-26 14:23:28 +0300

    404 page added

---
+++ Traversal/Controllers/ErrorPageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        public IActionResult Error404(int code)
+            //hata koduna göre döndürme işlemleri yapılabilir
+        {
+
+            return View();
+        }
+    }
+}
--- Traversal/Program.cs
+++ Traversal/Program.cs
@@ -75,6 +75,8 @@ if (!app.Environment.IsDevelopment())
 	app.UseHsts();
 }
 
+app.UseStatusCodePagesWithReExecute("/ErrorPage/Error404","?code={0}"); 
+//{0} dışarıdan bir kod alabilir demek
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
