1eb6f21e929f3cfd26f0fdda470b0f531da3b3d4
[buse duran] <buseduran500@gmail.com>
2023-04-09 23:14:26 +0300

    admin mail start

---
+++ Traversal/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,34 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using Traversal.Areas.Admin.Models;
+
+namespace Traversal.Areas.Admin.Controllers
+{
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+            MailboxAddress mailboxAddressFrom = new MailboxAddress(mailRequest.Name,mailRequest.Sender);//kimden gönderilcek
+            mimeMessage.From.Add(mailboxAddressFrom);
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailRequest.Receiver);
+
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            mimeMessage.Subject = mimeMessage.Subject;
+            SmtpClient client = new SmtpClient();
+            client.Connect("smptp.gmail.com", 587, false);
+            client.Authenticate(mailRequest.Sender, "123456Bb*");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+            return View();
+        }
+    }
+}
---
+++ Traversal/Areas/Admin/Models/MailRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Traversal.Areas.Admin.Models
+{
+    public class MailRequest
+    {
+        public string? Name { get; set; }
+        public string? Sender { get; set; }
+        public string? Receiver { get; set; }
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
+    }
+}
