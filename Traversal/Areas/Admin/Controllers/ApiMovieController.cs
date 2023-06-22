03f6c035ba09e90785bb00771e9fe4875659078b
[buse duran] <buseduran500@gmail.com>
2023-06-22 16:37:33 +0300

    rapidapi imdb top100

---
+++ Traversal/Areas/Admin/Controllers/ApiMovieController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Traversal.Areas.Admin.Models;
+
+namespace Traversal.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[AllowAnonymous]
+	public class ApiMovieController : Controller
+	{
+		
+		public async Task<IActionResult> Index()
+		{
+            List<ApiMovieViewModel> _apiMovies = new List<ApiMovieViewModel>();
+            var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://imdb-top-100-movies1.p.rapidapi.com/"),
+				Headers =
+	{
+		{ "X-RapidAPI-Key", "60a34c30e5msh48eebb101f4d624p1f8af9jsn8f9ae77c7d41" },
+		{ "X-RapidAPI-Host", "imdb-top-100-movies1.p.rapidapi.com" },
+	},
+			};
+			using (var response = await client.SendAsync(request))
+			{
+				response.EnsureSuccessStatusCode();
+				var body = await response.Content.ReadAsStringAsync();
+				_apiMovies = JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
+
+				return View(_apiMovies);
+			}
+
+		}
+	}
+}
---
+++ Traversal/Areas/Admin/Models/ApiMovieViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DocumentFormat.OpenXml.Office2013.Excel;
+
+namespace Traversal.Areas.Admin.Models
+{
+	public class ApiMovieViewModel
+	{
+        public int rank { get; set; }
+		public string? title { get; set; }
+		public string? description { get; set; }
+        public string? director { get; set; }
+        public string? genre { get; set; }
+        public int? year { get;set; } 
+        public string? rating { get; set; }
+        public string? trailer { get; set; }
+    
+    }
+}
