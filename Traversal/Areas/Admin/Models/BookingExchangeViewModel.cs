cb9076af49cc1c72d555d1693fb0f1a8124a9d0c
[buse duran] <buseduran500@gmail.com>
2023-06-25 22:32:06 +0300

    booking api added

---
+++ Traversal/Areas/Admin/Models/BookingExchangeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Traversal.Areas.Admin.Models
+{
+    public class BookingExchangeViewModel
+    {
+        public string? exchange_rate_buy { get; set; }
+        public string? currency { get; set; }
+
+    }
+}
