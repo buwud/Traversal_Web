03a3c1c0c8bb83e43345ade52716c0a0512a4c1a
[buse duran] <buseduran500@gmail.com>
2023-07-28 13:45:25 +0300

    default feature done, some edits

--- Traversal/ViewComponents/Default/_FeaturePartial.cs
+++ Traversal/ViewComponents/Default/_FeaturePartial.cs
@@ -14,23 +14,23 @@ namespace Traversal.ViewComponents.Default
 
             ViewBag.v1DayNite = values[0].StayTime;
             ViewBag.v1Price = values[0].Price;
-            ViewBag.v1Img = values[0].Image;
+            ViewBag.v1Img = values[0].ImageS;
 
             ViewBag.v2DayNite = values[1].StayTime;
             ViewBag.v2Price = values[1].Price;
-            ViewBag.v2Img = values[1].Image;
+            ViewBag.v2Img = values[1].ImageS;
                 
             ViewBag.v3DayNite = values[2].StayTime;
             ViewBag.v3Price = values[2].Price;
-            ViewBag.v3Img = values[2].Image;
+            ViewBag.v3Img = values[2].ImageS;
 
             ViewBag.v4DayNite = values[3].StayTime;
             ViewBag.v4Price = values[3].Price;
-            ViewBag.v4Img = values[3].Image;
+            ViewBag.v4Img = values[3].ImageS;
 
             ViewBag.v5DayNite = values[4].StayTime;
             ViewBag.v5Price = values[4].Price;
-            ViewBag.v5Img = values[4].Image;
+            ViewBag.v5Img = values[4].ImageS;
 
             return View();
         }//en az capacitysi olan 5 tane listelenecek
