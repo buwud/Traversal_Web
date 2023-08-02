51c867998b90ffb725af1cb854fca953c639d5f3
[buse duran] <buseduran500@gmail.com>
2023-08-02 13:59:45 +0300

    sql and design edits

--- DataAccessLayer/Migrations/ContextModelSnapshot.cs
+++ DataAccessLayer/Migrations/ContextModelSnapshot.cs
@@ -408,6 +408,9 @@ namespace DataAccessLayer.Migrations
                     b.Property<string>("Description")
                         .HasColumnType("nvarchar(max)");
 
+                    b.Property<string>("Description1")
+                        .HasColumnType("nvarchar(max)");
+
                     b.Property<string>("Image")
                         .HasColumnType("nvarchar(max)");
 
--- EntityLayer/Concrete/Guide.cs
+++ EntityLayer/Concrete/Guide.cs
@@ -14,6 +14,7 @@ namespace EntityLayer.Concrete
 		public string? Name { get; set; }
 		public string? Image { get; set; }
 		public string? Description { get; set; }
+		public string? Description1 { get; set; }
 		public string? TwitterURL { get; set; }
 		public string? InstagramURL { get; set; }
 		public bool Status { get; set; }
