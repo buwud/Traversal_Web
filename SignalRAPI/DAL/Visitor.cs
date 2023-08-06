79c0058606ebe46f3e4cf621fafef2b5bd13445f
[buse duran] <buseduran500@gmail.com>
2023-08-06 13:57:27 +0300

    postgreSql

---
+++ SignalRAPI/DAL/Visitor.cs
@@ -0,0 +1,19 @@
+﻿namespace SignalRAPI.DAL
+{
+    public enum Ecity
+    {
+        Berlin = 1,
+        İstanbul = 2,
+        Ankara = 3,
+        Kayseri = 4,
+        Maraş = 5
+
+    }
+    public class Visitor
+    {
+        public int visitorID { get; set; }
+        public Ecity ecity { get; set; }
+        public int cityVisitorCount { get; set; }
+        public DateTime dateTime  { get; set; }
+    }
+}
---
+++ SignalRAPI/Migrations/20230806100922_mig_first.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SignalRAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_first : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Visitors",
+                columns: table => new
+                {
+                    visitorID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ecity = table.Column<int>(type: "integer", nullable: false),
+                    cityVisitorCount = table.Column<int>(type: "integer", nullable: false),
+                    dateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Visitors", x => x.visitorID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Visitors");
+        }
+    }
+}
